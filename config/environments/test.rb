cb7194ff932416fc1f455ed30928044e4be57cde
mooradian79 <martin_mooradian@yahoo.com>
2012-04-11 13:32:30 -0400

    Added feature to ensure Devise signup is always working

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,8 @@
 Ticketee::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+ 
+	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+	
+	 # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
